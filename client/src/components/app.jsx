ba961646b41ab613fdde8e346e0cfb5a5553e39d
Roman Gorelik <romang31@gmail.com>
2018-08-08 10:50:00 -0400

    Created skeleton, react, express server

---
+++ client/src/components/app.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        <p>Hello Team</p>
+      </div>
+    )
+  }
+}
+
+export default App
