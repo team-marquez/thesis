fc95721a5aab6cb58adf25377a32a130d9f7503d
Martin <MartinGlyer@gmail.com>
2018-08-08 19:06:08 -0400

    minor add

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const morgan = require('morgan')
 const app = express()
 app.use(bodyParser.json());
 app.use(morgan('dev'))
-app.use(express.static(__dirname + '/client/dist/'))
+app.use(express.static(__dirname + '/../client/dist/'))
 
 app.get('/', (req, res) => {
   res.send('server working')
