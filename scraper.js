d5d1e13f30b76b07776039c404249ec847134cc3
Martin <MartinGlyer@gmail.com>
2018-08-09 16:32:14 -0400

    feat(root): creates an itial scrape of atlas obscura places in nyc
    includes a name, description, and img src for the first page

--- scraper.js
+++ scraper.js
@@ -7,10 +7,11 @@ let search_url = 'https://www.atlasobscura.com/things-to-do/new-york/places';
 axios.get(search_url).then((response) => {
     let $ = cheerio.load(response.data);
     let data = []
-    $('#page-content .geo-places .index-row-wrap .index-card-wrap').each((index, element) => {
+    $('.index-card-wrap').each((index, element) => {
         data[index] = {};
         data[index]['name'] = $(element).find('.content-card-title').text()
         data[index]['description'] = $(element).find('.js-subtitle-content').text()
+        data[index]['img'] = $(element).find('.content-card-figure img').attr('src')
     })
     return (data)
 }).then((data) => {
