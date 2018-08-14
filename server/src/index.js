7c9ff4a76ea8c426bf37a7a225e859dc9044b4b9
Erik <elin7780@bths.edu>
2018-08-14 16:25:27 -0400

    fix(Server): Query and mutations work for Prisma
    Fix problems sending queries and mutations to interact with PostgreSQL database

--- server/src/index.js
+++ server/src/index.js
@@ -1,49 +1,36 @@
 const { GraphQLServer } = require('graphql-yoga')
 const { Prisma } = require('prisma-binding')
-const express =  require('express')
+const express = require('express')
 const path = require('path')
-
-/*
-var typeDefs = gql`
-    type Query {
-      globalQuery(
-        budget {
-          alloc: String
-          amount: String
-          food: String
-          activities: String
-        }
-        in_out: String
-        local_tourist: String
-        food_activity: String
-        NSFW: String
-        days: String
-        travelers: String
-      )
-    }
-`;
-*/
-// var typeDefs = gql`
-//   type Query {
-//     globalQuery(request: String!): String
-//   }
-// `
 
 const resolvers = {
   Query: {
     users: (_, args, context, info) => {
       console.log(`info: ${Object.keys(context.db)}
       `)
-      return context.prisma.query.users
+      return context.db.query.users
     },
-    echo: (a, {args}, b , c) => {
+    echo: (a, { args }, b, c) => {
       console.log(args)
       return args
     }
-    },
-    Mutation: {
-      createUsers: (a,b,c,d) => console.log(`${JSON.stringify(Object.keys(c.response.output))}`)
+  },
+  
+  Mutation: {
+    createUsers: (_, { username, password, age, gender }, context, info) => {
+      return context.db.mutation.createUsers(
+        {
+          data: {
+            username,
+            password,
+            age,
+            gender
+          }
+        },
+        info
+      )
     }
+  }
 }
 
 const server = new GraphQLServer({
@@ -53,18 +40,22 @@ const server = new GraphQLServer({
     ...req,
     db: new Prisma({
       typeDefs: path.join(__dirname, '/generated/prisma.graphql'), // the auto-generated GraphQL schema of the Prisma API
-      endpoint: 'http://localhost:4466/graphql', // the endpoint of the Prisma API
+      endpoint: 'http://localhost:4466/', // the endpoint of the Prisma API
       debug: true // log all GraphQL queries & mutations sent to the Prisma API
       // secret: 'mysecret123', // only needed if specified in `database/prisma.yml`
     })
   })
 })
+
 server.express.use(express.static(__dirname + '/../../client/dist/'))
 
 const options = {
   port: 4000,
   endpoint: '/graphql',
   subscriptions: '/subscriptions',
-  playground: '/playground',
+  playground: '/playground'
 }
-server.start(options, ({port}) => console.log('Server is running on http://localhost:'+ port))
+
+server.start(options, ({ port }) =>
+  console.log('Server is running on http://localhost:' + port)
+)
