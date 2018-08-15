7a7d3a7253816fe5690a3ad402713288902b05c6
Martin <MartinGlyer@gmail.com>
2018-08-15 12:14:06 -0400

    feat(assembly_boi.js): writes out the FillMeal function
    this function, when run on a meal type, will populate the array on the tripOptions until it reaches
    its max size with appropriate rests.  It also switches between adding local and tourist spots until
    they reach their max allocation

--- server/src/bois/assembly_boi.js
+++ server/src/bois/assembly_boi.js
@@ -24,8 +24,26 @@ module.exports = {
     }
 
     fillMeals = (mealType) => {
-      
+      let currentLT = LTAllocation.local > LTAllocation.tourist ? 0 : 1
+      let localCount = 0
+      let touristCount = 0
+      for (let meal in recs) {
+        let type = recs[meal].type
+        if (type[0] === 'restaurant') {
+          if (type[1].meal.includes(mealType) && recs[meal].LT === currentLT) {
+            tripOptions[mealType].push(recs[meal])
+            if (tripOptions[mealType].length === arrayMax) break
+            currentLT === 0 ? localCount++ : touristCount++
+            currentLT = currentLT === 0 ? 1 : 0
+            if (localCount === LTAllocation.local || touristCount === LTAllocation.tourist) {
+              currentLT = currentLT === 0 ? 1 : 0
+            }
+          }
+        }
+      }
     }
+
+    
   }
 }
 
