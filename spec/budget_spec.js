4e26fafa07fde00c1a63e71834f1a067f6dcf1ff
Martin <MartinGlyer@gmail.com>
2018-08-18 12:54:59 -0400

    refactor(trip_logic, budget_boi): refactors the contract of objects
    also begins to rewrite budget boi for the new object

--- spec/budget_spec.js
+++ spec/budget_spec.js
@@ -3,32 +3,33 @@ const { budgetBoi } = require('./../server/src/bois/budget_boi.js')
 const { recsData, clientPreferances } = require('./dummyData.js')
 
 describe('BudgetBoi', () => {
-    describe('Structure', () => {
-        let output = budgetBoi(recsData, clientPreferances)
+    // describe('Structure', () => {
+    //     let output = budgetBoi(recsData, clientPreferances)
         
-        it('should return an object', () => {
-            expect(typeof output).to.equal('object')
-        })
+    //     it('should return an object', () => {
+    //         expect(typeof output).to.equal('object')
+    //     })
         
-    })
+    // })
     
     describe('Functionality', () => {
-        let output = budgetBoi(recsData, clientPreferances)
+        // let output = budgetBoi(recsData, clientPreferances)
 
         // it('should return a shorter object than the one it was fed', () => {
         //     expect(Object.keys(output).length).to.be.lessThan(Object.keys(recsData).length)
         // })
 
         it('should delete an activity if outside price range', () => {
-            let data = {Highline: {type: ['activity'], cost: 0}, ExpensivePlace: {type: ['activity'], cost: 5}}
-            budgetBoi(data, clientPreferances)
-            expect(data.hasOwnProperty('ExpensivePlace')).to.equal(false)
+            let data = [{name: 'Highline', type: ['activity'], cost: 0}, {name: 'ExpensivePlace', type: ['activity'], cost: 5}]
+            let output2 = budgetBoi(data, clientPreferances)
+            console.log(output2)
+            expect(output2.length).to.lessThan(data.length)
         })
-
+        
         it('should not delete an activity if inside price range', () => {
-            let data = {Highline: {type: ['activity'], cost: 0}, ExpensivePlace: {type: ['activity'], cost: 5}}
-            budgetBoi(data, clientPreferances)
-            expect(data.hasOwnProperty('Highline')).to.equal(true)
+            let data = [{name: 'Highline', type: ['activity'], cost: 0}, {name: 'ExpensivePlace', type: ['activity'], cost: 5}]
+            let output2 = budgetBoi(data, clientPreferances)
+            expect(output2[0].name).to.equal('Highline')
         })
 
     })
