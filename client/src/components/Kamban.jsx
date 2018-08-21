7459c93c69a7d733ec78265fcb19463c14d91f84
Roman Gorelik <romang31@gmail.com>
2018-08-21 11:40:02 -0400

    feat($Kamban.jsx): Added images and weather
    Added images based on the weather forecast for that day, added weather info on the top of the card.

--- client/src/components/Kamban.jsx
+++ client/src/components/Kamban.jsx
@@ -13,8 +13,8 @@ const getItems = (count, array) =>
     content: array[index].map((activity) => { return (  
       <div>
         <div>  
-          <div style={{float: 'right'}}>{activity.name}</div><br/>
-          <div style={{float: 'right'}}>{activity.cost === null || 0 ? 'Free' : activity.cost === 1 ? '$' : activity.cost === 2 ? '$$' : activity.cost === 3 ? '$$$' : activity.cost === 4 ? '$$$$' : null}</div>
+          <div style={{float: 'right', color: 'rgb(245, 255, 246)', fontWeight: 'bold', textShadow: '2px 0px black'}}>{activity.name}</div><br/>
+          <div style={{float: 'right', color: 'rgb(245, 255, 246)', fontWeight: 'bold', textShadow: '2px 0px black'}}>{activity.cost === null || 0 ? 'Free' : activity.cost === 1 ? '$' : activity.cost === 2 ? '$$' : activity.cost === 3 ? '$$$' : activity.cost === 4 ? '$$$$' : null}</div>
           <Image style={{width: '50px', height: '50px'}} src={activity.image}></Image>
         </div> <br/>
       </div>
@@ -39,14 +39,14 @@ const getItemStyle = (isDragging, draggableStyle) => ({
   margin: 0,
 
   // change background colour if dragging
-  background: isDragging ? 'lightgreen' : 'white',
+  background: isDragging ? 'lightgreen' : 'rgba(0,0,0,0.0)',
 
   // styles we need to apply on draggables
   ...draggableStyle,
 });
 
 const getListStyle = isDraggingOver => ({
-  background: isDraggingOver ? 'white' : 'white',
+  background: isDraggingOver ? 'rgba(0,0,0,0.0)' : 'rgba(0,0,0,0.0)',
   display: 'flex',
   padding: grid,
   overflow: 'auto',
@@ -100,9 +100,16 @@ class Kamban extends React.Component {
                   {...provided.droppableProps}
                 >
                   {this.state.items.map((item, index) => (
-                    <div  key = {index} style={{border: '2px solid gray', height: '700px', width: '300px', marginRight: '10px', float: 'right'}}>
-                      <div>
-                        {`${this.props.temp[index].avg_temp} degrees`}
+                    <div  key = {index} style={{border: '2px solid gray', height: '700px', width: '300px', marginRight: '10px', float: 'right', 
+                    backgroundImage: this.props.temp[index].rain_chance === 0 ? 'url(https://i.imgur.com/gf8W6wy.jpg)' : 'url(https://78.media.tumblr.com/a9207e4a4e7c4680611259bfd6f3d341/tumblr_ns4cghz1OP1u7nuo6o1_500.jpg)'}}>
+                      <div> 
+                        <div>
+                          {`${this.props.temp[index].max_temp.toString().substr(0, 2)}° Max || ${this.props.temp[index].min_temp.toString().substr(0, 2)}° Min`}
+                        </div>
+                        <div>
+                          {this.props.temp[index].rain_chance === 0 ? 'Little to No chance of rain' : 'High chance of rain'}
+                        </div>
+                        <hr></hr>
                       </div>
                       <br/>
 
@@ -119,7 +126,7 @@ class Kamban extends React.Component {
                             )}
                           >
                           <FlexyFlipCard
-                              frontBackgroundColor="white"
+                              frontBackgroundColor="rgba(0,0,0,0.0)"
                               backBackgroundColor="white"
                           >
                             <div>
