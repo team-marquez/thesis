3c013d0478dc482c337512e4514b8aac8f923440
Erik <elin7780@bths.edu>
2018-08-21 11:15:51 -0400

    Update to Webpack 4

--- spec/randomrecs.js
+++ spec/randomrecs.js
@@ -1,9 +1,11 @@
-const fs = require("fs")
-const shuffle = require("shuffle-array")
+const fs = require('fs')
+const shuffle = require('shuffle-array')
+const path = require('path')
+const DIR = path.join(__dirname, '../data.json')
 
 module.exports = {
   test: async () => {
-    let filehandle = await fs.readFileSync("./data.json")
+    let filehandle = await fs.readFileSync(DIR)
     let parsed = JSON.parse(filehandle)
     let random = shuffle(parsed, { copy: true })
     filehandle.close
