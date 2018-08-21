b7452f522c1017e2813fbe85a4ab1223b6441fdd
Martin <MartinGlyer@gmail.com>
2018-08-21 16:24:39 -0400

    feat(onboarding.jsx): creates the 6 info modals
    also adds state to open and close each one on button clicks, so no more than 1 is open at any
    moment.

--- client/src/components/Onboarding.jsx
+++ client/src/components/Onboarding.jsx
@@ -1,19 +1,48 @@
 import React from 'react'
-import  { Button, Modal, Header } from 'semantic-ui-react'
+import  { Button, Modal, Header, Icon } from 'semantic-ui-react'
 const { onboardingActivities } = require('./helpers/onboardingActivities.js')
 
 class Onboarding extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            onboardingArray: []
+            onboardingArray: [],
+            open: false,
+            showFirst: false,
+            showSecond: false,
+            showThird: false,
+            showFourth: false,
+            showFifth: false,
+            showSixth: false
         }
         this.fillOnboardingArray = this.fillOnboardingArray.bind(this)
+        this.open = this.open.bind(this)
+        this.close = this.close.bind(this)
+        this.showFirst = this.showFirst.bind(this)
+        this.showSecond = this.showSecond.bind(this)
+        this.showThird = this.showThird.bind(this)
+        this.showFourth = this.showFourth.bind(this)
+        this.showFifth = this.showFifth.bind(this)
+        this.showSixth = this.showSixth.bind(this)
+        this.submitAnswers = this.submitAnswers.bind(this)
     }
-
     componentWillMount() {
       this.fillOnboardingArray()
     }
+
+    open(){ this.setState({ open: true }) }
+    close() { this.setState({ open: false }, this.showFirst()) }
+
+    showFirst() {this.setState({ showFirst: true})}
+    showSecond() {this.setState({ showFirst: false, showSecond: true})}
+    showThird() {this.setState({ showSecond: false, showThird: true})}
+    showFourth() {this.setState({ showThird: false, showFourth: true})}
+    showFifth() {this.setState({ showFourth: false, showFifth: true})}
+    showSixth() {this.setState({ showFifth: false, showSixth: true})}
+    submitAnswers() {
+      this.setState({ showSixth: false})
+    }
+
 
     fillOnboardingArray() {
       let chosenIndicies = {};
@@ -27,9 +56,10 @@ class Onboarding extends React.Component {
     }
     
     render() {
+      const { open } = this.state
       return (
         <div>
-          <Modal size = {'fullscreen'} trigger= {<Button>Display Onboarding</Button>} >
+          <Modal open={open} size = {'fullscreen'} trigger= {<Button onClick = {this.open} >Display Onboarding</Button>} >
             <Header content={`Welcome to Let's Go To!`} />
             <Modal.Content>
               <p>
@@ -42,6 +72,77 @@ class Onboarding extends React.Component {
                 To get started, just press Next!
               </p>
             </Modal.Content>
+            <Modal.Actions>
+              <Button onClick={this.close} >
+                Next <Icon name='right chevron'/>
+              </Button>
+            </Modal.Actions>
+          </Modal>
+
+          <Modal open = {this.state.showFirst}>
+            <Modal.Content>
+              here i am to save the daaaaaay!
+            </Modal.Content>
+            <Modal.Actions>
+              <Button onClick={this.showSecond} >
+                Next <Icon name='right chevron'/>
+              </Button>
+            </Modal.Actions>
+          </Modal>
+
+          <Modal open = {this.state.showSecond}>
+            <Modal.Content>
+              data number 2
+            </Modal.Content>
+            <Modal.Actions>
+              <Button onClick={this.showThird} >
+                Next <Icon name='right chevron'/>
+              </Button>
+            </Modal.Actions>
+          </Modal>
+
+          <Modal open = {this.state.showThird}>
+            <Modal.Content>
+              data number threeeee
+            </Modal.Content>
+            <Modal.Actions>
+              <Button onClick={this.showFourth} >
+                Next <Icon name='right chevron'/>
+              </Button>
+            </Modal.Actions>
+          </Modal>
+
+          <Modal open = {this.state.showFourth}>
+            <Modal.Content>
+              44444
+            </Modal.Content>
+            <Modal.Actions>
+              <Button onClick={this.showFifth} >
+                Next <Icon name='right chevron'/>
+              </Button>
+            </Modal.Actions>
+          </Modal>
+
+          <Modal open = {this.state.showFifth}>
+            <Modal.Content>
+              5 5 5 5 5 5
+            </Modal.Content>
+            <Modal.Actions>
+              <Button onClick={this.showSixth} >
+                Next <Icon name='right chevron'/>
+              </Button>
+            </Modal.Actions>
+          </Modal>
+
+          <Modal open = {this.state.showSixth}>
+            <Modal.Content>
+              666 is evil!
+            </Modal.Content>
+            <Modal.Actions>
+              <Button onClick={this.submitAnswers} >
+              <Icon name='checkmark'/>   Thank You! 
+              </Button>
+            </Modal.Actions>
           </Modal>
         </div>
       )
