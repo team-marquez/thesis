46567c377504e0d91ddc4f3165ab28881dbda81f
Roman Gorelik <romang31@gmail.com>
2018-08-22 12:00:25 -0400

    feat($app, $login): Added login/logout dynamics, user page button
    Doesnt show logout when not logged in. Changes log in button to logout when logged in. Adds a user
    page where the logout previously was when logged in.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,16 +4,19 @@ import LoginButton from "./LoginButton.jsx";
 import LocationModal from "./LocationModal.jsx";
 import Footer from "./Footer.jsx";
 import AllDays from "./AllDays.jsx";
-
 import Onboarding from './Onboarding.jsx'
+
+import { Button } from 'semantic-ui-react'
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      pickedTrip: false
+      pickedTrip: false,
+      home: false
     };
-    this.pickTrip = this.pickTrip.bind(this);
+    this.pickTrip = this.pickTrip.bind(this)
+    this.switchHome = this.switchHome.bind(this)
   }
 
   // Conditional rendering function for displaying the home page and the trip page. Works on LocationModals 'Next' button.
@@ -23,27 +26,38 @@ class App extends React.Component {
     });
   }
 
+  switchHome() {
+    this.setState({
+      home: !this.state.home
+    })
+  }
+
   render() {
     return (
       <div>
-        {this.state.pickedTrip === false ? (
+        {this.state.home === false ? (
           <div>
-            <div style={{textAlign: 'center', marginBottom: '15px'}}>
-              <LoginButton/>  
-            </div>
-            <div style={{display: 'inline-block'}}>
-              <LocationModal pickTrip={this.pickTrip}/>
-            </div>
-            <Footer/>
-            <Onboarding />
-          </div>
-        ) : (
-          <div style={{textAlign: 'center'}}>
-            <AllDays/>
-          </div>
-        )}
+            {this.state.pickedTrip === false ? (
+              <div>
+                <div style={{textAlign: 'center', marginBottom: '15px'}}>
+                  <LoginButton/>  
+                </div>
+                <div style={{display: 'inline-block'}}>
+                  <LocationModal pickTrip={this.pickTrip}/>
+                </div>
+                  <Footer/>
+                  <Onboarding/>
+                  <Button onClick = {this.switchHome}></Button>
+              </div>
+            ) : (
+              <div style={{textAlign: 'center'}}>
+                <AllDays/>
+              </div>
+            )}
+        </div>
+        ) : (<div> <Button onClick = {this.switchHome}></Button></div>)}
       </div>
-    );
+    )
   }
 }
 
