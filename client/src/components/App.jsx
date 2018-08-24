bba203c2467ae02ff6913b062fafb3539d883f87
Erik <elin7780@bths.edu>
2018-08-24 14:09:04 -0400

    Set up for sidebar display for logged in users

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Icon } from 'semantic-ui-react'
+import { Button, Icon, Sidebar, Menu } from 'semantic-ui-react'
 
 import LoginButton from "./LoginButton.jsx";
 import Footer from "./Footer.jsx";
@@ -17,13 +17,20 @@ class App extends React.Component {
       home: false,
       user: 'Welcome User',
       image: 'https://react.semantic-ui.com/images/avatar/large/patrick.png',
-      openOnboarding: false
+      openOnboarding: false,
+      visible: false,
+      login: false
     };
     this.pickTrip = this.pickTrip.bind(this)
     this.switchHome = this.switchHome.bind(this)
     this.changeUser = this.changeUser.bind(this)
     this.openOnboarding = this.openOnboarding.bind(this)
     this.closeFirstOnboard = this.closeFirstOnboard.bind(this)
+
+    this.handleButtonClick = this.handleButtonClick.bind(this)
+    this.handleSidebarHide = this.handleSidebarHide.bind(this)
+    this.handleLogin = this.handleLogin.bind(this)
+    this.handleLogout = this.handleLogout.bind(this)
   }
 
   // Conditional rendering function for displaying the home page and the trip page. Works on LocationModals 'Next' button.
@@ -47,17 +54,41 @@ class App extends React.Component {
     })
   }
 
-  openOnboarding () {
+  openOnboarding() {
     this.setState({openOnboarding: true})
   }
 
-  closeFirstOnboard () {
+  closeFirstOnboard() {
     this.setState({openOnboarding: false})
+  }
+
+  handleButtonClick() {
+    this.setState({visible: !this.state.visible})
+  }
+
+  handleSidebarHide() {
+    this.setState({visible: false})
+  }
+
+  handleLogin() {
+    this.setState({login: true})
+  }
+
+  handleLogout() {
+    this.setState({login: false})
   }
 
   render() {
     return (
       <div>
+        <Sidebar.Pushable>
+          <Sidebar as={Menu} animation='overlay' onHide={this.handleSidebarHide} vertical visible={this.state.visible} width='thin'>
+            <Menu.Item>Home</Menu.Item>
+            <Menu.Item>Profile</Menu.Item>
+            <Menu.Item as='a' onClick={this.handleLogout}>Logout</Menu.Item>
+          </Sidebar>
+
+          <Sidebar.Pusher>
         {this.state.home === false ? (
           <div>
             {this.state.pickedTrip === false ? (
@@ -65,9 +96,10 @@ class App extends React.Component {
 
                 <img style={{ minWidth: '100%', minHeight: '100%', filter:'grayscale(100%)' }} src="https://images.unsplash.com/photo-1523756025758-565a549d6eb6?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9&s=d8663c5594055b93eb4401194c780668" />
 
-                <div style={{position: 'absolute', top: '4%', left: '86%'}}>
+                <div>
                   <LoginButton home={this.switchHome} user={this.state.user} image={this.state.image} changeUser={this.changeUser}
-                    openOnboarding={this.openOnboarding}/>
+                    openOnboarding={this.openOnboarding} handleButtonClick={this.handleButtonClick} handleLogin={this.handleLogin}
+                    handleLogout={this.handleLogout} loggedIn={this.state.login}/>
                 </div>
 
                 <div style={{position: 'absolute', top: '50%', left: '50%', font: 'arial', color: 'white', fontSize: '100px', fontWeight: 'bold', transform: 'translate(-50%, -50%)'}}>
@@ -76,17 +108,7 @@ class App extends React.Component {
                     <UserPreferences pickTrip={this.pickTrip} />
                   </div>
                 </div>
-
-                {/* <div style={{textAlign: 'center', marginBottom: '15px'}}>
-                  <LoginButton home={this.switchHome} user={this.state.user} image={this.state.image} changeUser={this.changeUser}
-                    openOnboarding = {this.openOnboarding}
-                  />  
-                </div>
-                <div style={{display: 'inline-block'}}>
-                  <LocationModal pickTrip={this.pickTrip}/>
-                </div>
-                  <Footer/>
-                  <Onboarding open={this.state.openOnboarding} closer = {this.closeFirstOnboard} /> */}
+                  {/* <Onboarding open={this.state.openOnboarding} closer = {this.closeFirstOnboard} /> */} */}
               </div>
             ) : (
               <div style={{textAlign: 'center'}}>
@@ -97,6 +119,8 @@ class App extends React.Component {
         ) : (
           <UserProfile home={this.switchHome} user={this.state.user}/>
         )}
+          </Sidebar.Pusher>
+      </Sidebar.Pushable>
       </div>
     )
   }
