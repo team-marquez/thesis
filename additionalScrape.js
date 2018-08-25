3ecd5aac31bcf1edcfd6a7f57838c1ae60adfc39
Martin <MartinGlyer@gmail.com>
2018-08-25 16:11:05 -0400

    feat(additionalScrape.js): scrapes from ny.eater for breakfast spots

--- additionalScrape.js
+++ additionalScrape.js
@@ -8,22 +8,7 @@ const prisma = new Prisma({
   endpoint: "http://localhost:4466/"
 });
 
-const Nightmare = require("nightmare");
-const nightmare = Nightmare({ show: true });
-
-const breakfastScrape2 = website => {
-  let item = {}
-  nightmare
-    .goto(website)
-    .wait(3000)
-    .evaluate(() => info.name = document.querySelector('.zgt-place-sheet-title'.text()))
-    .end()
-    .then((data)  => {
-      return data
-    })
-    .catch(e => console.log(e))
-};
-
+//Zagat not functioning
 const breakfastScrape = website => {
   return axios.get(website).then(response => {
     let $ = cheerio.load(response.data);
@@ -56,4 +41,26 @@ const breakfastScrape = website => {
 //   "https://www.zagat.com/r/normas-at-le-parker-meridien-new-york-new-york"
 // ).then(data => console.log(data));
 
-console.log(breakfastScrape2('https://www.zagat.com/r/normas-at-le-parker-meridien-new-york-new-york'))
+// console.log(breakfastScrape2('https://www.zagat.com/r/normas-at-le-parker-meridien-new-york-new-york'))
+
+const nyEatBreakfast = (website) => {
+  return axios.get(website).then(response => {
+    let data = []
+    let $ = cheerio.load(response.data)
+    $('.c-mapstack__card').each((ind, el) => {
+      item = {}
+      item.name = $(el).find('h1').text().split('. ')[1]
+      item.address = $(el).find('.c-mapstack__address').text().trim()
+      item.website = $(el).find('.c-mapstack__phone-url').children().last().attr('href')
+      item.description = $(el).find('.c-entry-content p').text().trim()
+      item.image = $(el).find('picture img').attr('src')
+      let determine = Math.random()
+      item.cost = determine > .7 ? 2 : 1
+      item.local_tourist = Math.round(Math.random())
+      data.push(item)
+    })
+    return data
+  })
+}
+
+// nyEatBreakfast('https://ny.eater.com/maps/best-breakfast-nyc').then(thing => console.log(thing))
