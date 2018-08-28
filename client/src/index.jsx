0121a080da915ecd0ff9f5fd077741b5cb378c95
Erik <elin7780@bths.edu>
2018-08-28 17:18:21 -0400

    Begin refactor to use React Router

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+
 import App from './components/App.jsx'
 
 import { ApolloProvider } from 'react-apollo'
@@ -7,17 +9,21 @@ import ApolloClient from 'apollo-boost'
 
 const defaults = {
   itinerary: '',
-  userId: 'anon',
-};
+  userId: 'anon'
+}
 
-const client = new ApolloClient({ uri: 'http://localhost:4000/graphql', clientState: {defaults} })
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  clientState: { defaults }
+})
 
 render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById('app')
 )
-
 
 export default client
